0e520591185419b01a1c76b9e80962efa3d340c3
ZakFahey <z.fahey@gmail.com>
2018-04-26 17:30:26 -0400

    Attempt to load organizations from API

--- update-organizations/dist/index.js
+++ update-organizations/dist/index.js
@@ -1,36 +1,43 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const db_1 = require("../../db");
+const axios_1 = require("axios");
 const winston_1 = require("winston");
 require("winston-daily-rotate-file");
+const logger = new winston_1.Logger({
+    transports: [
+        new winston_1.transports.Console({
+            prepend: true,
+            level: 'debug',
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        }),
+        new winston_1.transports.DailyRotateFile({
+            filename: `./logs/app.log`,
+            datePattern: 'yyyy-MM-dd.',
+            createTree: true,
+            prepend: true,
+            level: 'debug',
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: true
+        })
+    ]
+});
 async function fetchOrganizations() {
-    return [{ id: '1', name: 'Test' }, { id: '2', name: 'Test3' }];
+    logger.info('Renewing API auth');
+    await axios_1.default.put('https://crossbar.reper.io/v2/user_auth', {
+        credentials: process.env.CREDENTIALS,
+        account_name: process.env.ACCOUNT_NAME
+    });
+    logger.info('Fetching new organizations');
+    const descendants = await axios_1.default.get(`https://crossbar.reper.io/v2/accounts/${process.env.account_id}/descendants`);
+    return descendants.data;
 }
 async function syncOrganizations() {
-    const logger = new winston_1.Logger({
-        transports: [
-            new winston_1.transports.Console({
-                prepend: true,
-                level: 'debug',
-                humanReadableUnhandledException: true,
-                handleExceptions: true,
-                json: false,
-                colorize: true
-            }),
-            new winston_1.transports.DailyRotateFile({
-                filename: `./logs/app.log`,
-                datePattern: 'yyyy-MM-dd.',
-                createTree: true,
-                prepend: true,
-                level: 'debug',
-                humanReadableUnhandledException: true,
-                handleExceptions: true,
-                json: true
-            })
-        ]
-    });
     const uow = new db_1.UnitOfWork(logger);
-    logger.info('Fetching new organizations');
     const organizations = await fetchOrganizations();
     logger.info('Adding new organizations to the database');
     let queries = [];
--- update-organizations/src/index.ts
+++ update-organizations/src/index.ts
@@ -3,36 +3,44 @@ import axios from 'axios';
 import {Logger, transports} from 'winston';
 import 'winston-daily-rotate-file';
 
+const logger = new Logger({
+    transports: [
+        new transports.Console({
+            prepend: true,
+            level: 'debug',
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: false,
+            colorize: true
+        }),
+        new transports.DailyRotateFile({
+            filename: `./logs/app.log`,
+            datePattern: 'yyyy-MM-dd.',
+            createTree: true,
+            prepend: true,
+            level: 'debug',
+            humanReadableUnhandledException: true,
+            handleExceptions: true,
+            json: true
+        })
+    ]
+});
+
 async function fetchOrganizations() : Promise<any[]> {
-    return [{id: '1', name: 'Test'}, {id: '2', name: 'Test3'}];
+    logger.info('Renewing API auth');
+    await axios.put('https://crossbar.reper.io/v2/user_auth', {
+        credentials: process.env.CREDENTIALS,
+        account_name: process.env.ACCOUNT_NAME
+    });
+
+    logger.info('Fetching new organizations');
+    const descendants = await axios.get(`https://crossbar.reper.io/v2/accounts/${process.env.account_id}/descendants`);
+    return descendants.data;
 }
 
 async function syncOrganizations() : Promise<void> {
-    const logger = new Logger({
-        transports: [
-            new transports.Console({
-                prepend: true,
-                level: 'debug',
-                humanReadableUnhandledException: true,
-                handleExceptions: true,
-                json: false,
-                colorize: true
-            }),
-            new transports.DailyRotateFile({
-                filename: `./logs/app.log`,
-                datePattern: 'yyyy-MM-dd.',
-                createTree: true,
-                prepend: true,
-                level: 'debug',
-                humanReadableUnhandledException: true,
-                handleExceptions: true,
-                json: true
-            })
-        ]
-    });
     const uow = new UnitOfWork(logger);
 
-    logger.info('Fetching new organizations');
     const organizations = await fetchOrganizations();
 
     logger.info('Adding new organizations to the database');
