50d5b1c492a198a5d51cab599c243bdefcf87cf2
ZakFahey <z.fahey@gmail.com>
2018-05-18 14:07:07 -0400

    Added firmware routes

---
+++ provisioning-server/src/api/firmwareRoutes.ts
@@ -0,0 +1,31 @@
+import {Request} from 'hapi';
+import {readFileSync, existsSync} from 'fs';
+
+const routes : any[] = [
+    {
+        method: 'GET',
+        path: '/{model}.sip.ld',
+        handler: async (request: Request, h: any) => {
+            const file = `./static/firmware/${request.params.model}.sip.ld`;
+            if(existsSync(file)) {
+                return readFileSync(file);
+            }
+            return h.response().code(404);
+        },
+        config: {
+            auth: false
+        }
+    },
+    {
+        method: 'GET',
+        path: '/{model}.bootrom.ld',
+        handler: async (request: Request, h: any) => {
+            return readFileSync(`./static/${request.params.model}.bootrom.ld`); //Add actual file
+        },
+        config: {
+            auth: false
+        }
+    }
+];
+
+export default routes;
