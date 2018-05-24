211ba9340003019245f41f247dafbc21d2556baa
ZakFahey <z.fahey@gmail.com>
2018-05-24 12:09:14 -0400

    More logging, updated config-conversion/package.json

---
+++ config-conversion/src/index.ts
@@ -0,0 +1,3 @@
+export * from './converters';
+export * from './getModelIDFromPath';
+export * from './propertyBuilder';
--- provisioning-server/src/api/firmwareRoutes.ts
+++ provisioning-server/src/api/firmwareRoutes.ts
@@ -6,6 +6,10 @@ const routes : any[] = [
         method: 'GET',
         path: '/{model}.sip.ld',
         handler: async (request: Request, h: any) => {
+            const logger = request.server.app.logger;
+
+            logger.debug(`Fetching ${request.params.model}.sip.ld.`);
+
             const file = `./static/firmware/${request.params.model}.sip.ld`;
             if(existsSync(file)) {
                 return readFileSync(file);
@@ -20,6 +24,10 @@ const routes : any[] = [
         method: 'GET',
         path: '/{model}.bootrom.ld',
         handler: async (request: Request, h: any) => {
+            const logger = request.server.app.logger;
+
+            logger.debug(`Fetching ${request.params.model}.bootrom.ld.`);
+
             return readFileSync(`./static/bootrom/${request.params.model}.bootrom.ld`); //Add actual file
         },
         config: {
